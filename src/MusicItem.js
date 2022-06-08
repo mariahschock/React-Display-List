96beaa04f33e941d8d58f052902a84e85ac9a491
Mariah <mariahschock@gmail.com>
2022-06-08 16:20:50 -0700

    error fix

--- src/MusicItem.js
+++ src/MusicItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function MusicItem({ genre, artist, TopSongs }) {
   return (
-    <div className="musics">
+    <div>
       <h2>Genre: {genre}</h2>
       <h3>Artist: {artist}</h3>
       <p>Top Songs:</p>
